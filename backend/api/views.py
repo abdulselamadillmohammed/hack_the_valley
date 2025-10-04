25e899e10abc09b3e8575a483ec00f16a94487e7
abdulselamadillmohammed <forcoding39@gmail.com>
2025-10-04 11:48:08 -0400

    congigure custom user

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from rest_framework import generics, permissions, status
+from django.contrib.auth import get_user_model
+from .serializers import RegisterSerializer
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from django.utils import timezone
+
+#token_authentication = ['rest_framework.authentication.TokenAuthentication']
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework_simplejwt.authentication import JWTAuthentication  # Import the correct class
+
+User = get_user_model()
+
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = RegisterSerializer
+    permission_classes = (permissions.AllowAny,)
